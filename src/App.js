ca98ff9b829b992624ef24f810a24270a38ad817
x <x@LAPTOP-SIFFNT03>
2020-06-02 16:57:47 -0600

    remove boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Table } from "reactstrap";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container">
+      <Table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>1</td>
+          <td>Bob</td>
+          <td>23</td>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default App;
